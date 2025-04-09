c29fc344273b06b06118e90b47366958f0991910
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-04-09 22:06:25 +0700

    Changed index to php

--- BetterHealth/html/index.php
+++ BetterHealth/html/index.php
@@ -42,7 +42,7 @@
                <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link" href="index.html">Home</a>
                      </li>
                      <li class="nav-item">
                         <a class="nav-link" href="#about_us">About Us</a>
