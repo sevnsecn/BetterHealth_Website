2644946c17e8b895d193b3282e721093c63173d7
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-04-09 22:12:01 +0700

    moved files around

---
+++ BetterHealth/html/signup.php
@@ -0,0 +1,32 @@
+<!-- Barebones -->
+
+<!DOCTYPE html>
+<html>
+  <head> 
+    <title> Signup </title> 
+    <style>
+        form{
+            display: flex;
+            flex-direction: column;
+            gap: 7px;
+            max-width: 300px;
+        }
+    </style>
+</head>
+  <body>
+    <form action="action.php" method="post"> 
+      <label for="username"> Enter Your Username: </label>
+      <input name="username" type="text" id="username" required>
+
+      <label for="password"> Enter a Password: </label>
+      <input name="password" type="password" id="password" required minlength="8">
+      <small style="color: gray;">Password must be at least 8 characters long.</small>
+
+      <label for="email"> Email: </label>
+      <input name="email" type="email" id="email" required>
+
+      <input type="submit" value="submit">
+      
+    </form>
+  </body>
+</html>
